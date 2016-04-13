ffd83bcec97b9d7a212f455d291532e35afe8d60
Felipe Martins <me@fefas.net>
2016-04-12 23:59:17 -0300

    add script to stalk the main conversation

---
+++ whatsappWebMainWindowStalker.js
@@ -0,0 +1,49 @@
+var lastMainStatus = undefined;
+var lastMainName = undefined;
+var mainStop = false;
+
+var main = function () {
+
+   if (mainStop) {
+      return;
+   }
+
+   var spanName   = $('#main div.chat-body div.chat-main span');
+   var spanStatus = $('#main div.chat-body div.chat-status span');
+
+   var name = spanName.textContent;
+   var open = false;
+
+   if (lastMainName != name) {
+      open = true;
+      lastMainName = name;
+      lastMainStatus = undefined;
+   }
+
+   var status = lastMainStatus;
+
+   if (spanStatus) {
+      status = spanStatus.textContent;
+   } else {
+      status = 'offline';
+   }
+
+   if (status != lastMainStatus) {
+      var d = new Date();
+      if (open) {
+         console.log('!>', name, '-', d.toLocaleTimeString(), '-', status);
+      } else {
+         console.log('*>', name, '-', d.toLocaleTimeString(), '-', status);
+      }
+      lastMainStatus = status;
+   }
+}
+
+var mainForEver = function () {
+   main();
+   setTimeout(mainForEver, 100);
+}
+
+var mainRun = function () {
+   setTimeout(mainForEver, 100);
+}
